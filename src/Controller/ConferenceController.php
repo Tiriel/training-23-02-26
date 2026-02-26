e4c406df9754024674379c438613cd0a19c76236
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-02-26 12:06:45 +0100

    Exercise 2 : Use DatabaseConferenceSearch

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,22 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Conference;
 use App\Form\ConferenceType;
-use App\Repository\ConferenceRepository;
+use App\Search\DatabaseConferenceSearch;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Requirement\Requirement;
 
 #[Route('/conference')]
 class ConferenceController extends AbstractController
 {
     #[Route('', name: 'app_conference_list', methods: ['GET'])]
-    public function list(ConferenceRepository $repository): Response
+    public function list(Request $request, DatabaseConferenceSearch $search): Response
     {
+        $name = $request->query->get('name');
+
         return $this->render('conference/list.html.twig', [
-            'conferences' => $repository->findAll(),
+            'conferences' => $search->searchByName($name),
         ]);
     }
 
